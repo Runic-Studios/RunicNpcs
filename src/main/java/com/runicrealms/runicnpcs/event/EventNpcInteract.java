b5bd1d1837b425406aadbfaafa5e169964b2cbe9
Skyfallin <dimetrius.hightower@gmail.com>
2021-08-30 16:29:38 -0700

    fixed multiple packets being listened for by filtering entity use actions and offhand

--- src/main/java/com/runicrealms/runicnpcs/event/EventNpcInteract.java
+++ src/main/java/com/runicrealms/runicnpcs/event/EventNpcInteract.java
@@ -5,10 +5,12 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.runicrealms.runicnpcs.Npc;
 import com.runicrealms.runicnpcs.Plugin;
 import com.runicrealms.runicnpcs.api.NpcClickEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 
 /**
@@ -22,6 +24,9 @@ public class EventNpcInteract implements Listener {
             public void onPacketReceiving(PacketEvent event) {
                 if (event.getPacketType() == PacketType.Play.Client.USE_ENTITY) {
                     PacketContainer packet = event.getPacket();
+                    if (packet.getEntityUseActions().readSafely(0) == EnumWrappers.EntityUseAction.ATTACK) return;
+                    if (packet.getEntityUseActions().readSafely(0) == EnumWrappers.EntityUseAction.INTERACT_AT) return;
+                    if (packet.getHands().readSafely(0) == EnumWrappers.Hand.OFF_HAND) return;
                     int entityID = packet.getIntegers().read(0);
                     Npc npc = Plugin.getNpcEntities().get(entityID);
                     if (npc == null) return;
