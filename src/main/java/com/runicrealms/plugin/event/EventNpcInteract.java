776a8245e5c79de8ad5d5e20e9fa4ecdc2e07b33
Skyfallin <skyfallin.business@gmail.com>
2023-02-16 09:01:53 -0500

    Added a feature to disable PlayerInteractEvent when talking to NPCs using DENY result!

--- src/main/java/com/runicrealms/plugin/event/EventNpcInteract.java
+++ src/main/java/com/runicrealms/plugin/event/EventNpcInteract.java
@@ -10,12 +10,24 @@ import com.runicrealms.plugin.Npc;
 import com.runicrealms.plugin.RunicNpcs;
 import com.runicrealms.plugin.api.NpcClickEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Handles the creation of our custom NpcClickEvent
+ * <p>
+ * Visit <a href="https://ci.dmulloy2.net/job/ProtocolLib/javadoc/com/comphenix/protocol/PacketType.html">ProtocolLib Docs</a>
  */
 public class EventNpcInteract implements Listener {
+    private final Set<UUID> npcTalkers = new HashSet<>();
 
     public EventNpcInteract() {
         RunicNpcs.getProtocolManager().addPacketListener(new PacketAdapter(RunicNpcs.getInstance(), ListenerPriority.NORMAL, PacketType.Play.Client.USE_ENTITY) {
@@ -29,11 +41,22 @@ public class EventNpcInteract implements Listener {
                     int entityID = packet.getIntegers().read(0);
                     Npc npc = RunicNpcs.getNpcEntities().get(entityID);
                     if (npc == null) return;
+                    npcTalkers.add(event.getPlayer().getUniqueId());
                     Bukkit.getScheduler().scheduleSyncDelayedTask(RunicNpcs.getInstance(),
                             () -> Bukkit.getServer().getPluginManager().callEvent(new NpcClickEvent(npc, event.getPlayer())));
                 }
             }
         });
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (npcTalkers.contains(event.getPlayer().getUniqueId())) {
+            event.setUseInteractedBlock(Event.Result.DENY);
+            event.setUseItemInHand(Event.Result.DENY);
+            npcTalkers.remove(event.getPlayer().getUniqueId());
+        }
     }
 
 }
